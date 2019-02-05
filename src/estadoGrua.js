1a7e54e8adbd5e3016d30410759d9f6bafc7159c
Randhall Rangel <rrangel@est.frba.utn.edu.ar>
2019-02-05 12:30:12 -0300

    Fix Test Estado Grua

--- src/estadoGrua.js
+++ src/estadoGrua.js
@@ -1,29 +1,27 @@
-  var estado = {
-    ACTIVO : {value: 0, name: "Activo", code: "A"}, 
-    DESACTIVO: {value: 1, name: "Desactivo", code: "D"}
-  };
+var estado = {
+  ACTIVO: { value: 0, name: "Activo", code: "A" },
+  DESACTIVO: { value: 1, name: "Desactivo", code: "D" }
+};
 
-export class estadoGrua{
-
-    /*
-    * Constructor de la Clase Estado de a Grua
-    */
-    constructor(nuevoEstado) {
-        super(nuevoEstado);
-        if (!nuevoEstado) {
-            throw new Error("Debe especificar el estado de la grua");
-        }
-        this.estado = nuevoEstado.DESACTIVO;
+export class estadoGrua {
+  /*
+   * Constructor de la Clase Estado de a Grua
+   */
+  constructor(nuevoEstado) {
+    if (!nuevoEstado) {
+      throw new Error("Debe especificar el estado de la grua");
     }
+    this.estado = nuevoEstado.DESACTIVO;
+  }
 
-    /**
-     * Devuelve las caracteristicas de la grua.
-     */
-    get getEstadoDeGrua() {
-        return this.estado;
-    }
+  /**
+   * Devuelve las caracteristicas de la grua.
+   */
+  get getEstadoDeGrua() {
+    return this.estado;
+  }
 
-    set setEstadoDeGrua(nuevoEstado){
-        this.estado = nuevoEstado;
-    }
+  set setEstadoDeGrua(nuevoEstado) {
+    this.estado = nuevoEstado;
+  }
 }
--- tests/estadoGrua.spec.js
+++ tests/estadoGrua.spec.js
@@ -1,27 +1,17 @@
-import { expect } from 'chai';
+import { expect } from "chai";
 
-describe('estadoGrua', function() {
-
-    context('constructor', function() {
-
-        it('should have a non null description', function() {
-            expect(function() {
-                const sut = new estadoGrua(null);
-            }).to.throw();
-        });
-
-        it ('should have a non empty description', function() {
-            expect(function() {
-                const sut = new estadoGrua('');
-            }).to.throw();
-        });
-
+describe("estadoGrua", function() {
+  context("constructor", function() {
+    it("should have a non null description", function() {
+      expect(function() {
+        const sut = new estadoGrua(null);
+      }).to.throw();
     });
 
-    context('getEstadoDeGrua', function() {
-        it ('should return empty', function() {
-          const sut = new estadoGrua('Desactivo');
-          //expect(sut.getEstadoDeGrua).to.be.equals(sut.estadoGrua == "" : "Error al obtener el estado d ela grua"?: );
-        });
+    it("should have a non empty description", function() {
+      expect(function() {
+        const sut = new estadoGrua("");
+      }).to.throw();
     });
+  });
 });
