a41df737a13a0f61cbaba578b244100f04d1e23d
Randhall Rangel <rrangel@est.frba.utn.edu.ar>
2019-02-09 00:08:39 -0300

    Class Test

--- tests/grua.spec.js
+++ tests/grua.spec.js
@@ -1,28 +1,27 @@
-import { expect } from 'chai';
-import { grua } from '../src/grua.js';
+import { expect } from "chai";
+import { grua } from "../src/grua.js";
 
-describe('grua', function() {
-
-    context('constructor', function() {
-
-        it('should have a non null description', function() {
-            expect(function() {
-                const sut = new grua(null);
-            }).to.throw();
-        });
-
-        it ('should have a non empty description', function() {
-            expect(function() {
-                const sut = new grua('');
-            }).to.throw();
-        });
-
+describe("grua", function() {
+  context("constructor", function() {
+    it("should have a non null description", function() {
+      expect(function() {
+        const sut = new grua(null);
+      }).to.throw();
     });
 
-    context('caracteristicas', function() {
-        it ('should return empty array', function() {
-          const sut = new grua('miGrua');
-          expect(sut.caracteristicas).to.be.an('array').lengthOf(0);
-        });
+    it("should have a non empty description", function() {
+      expect(function() {
+        const sut = new grua("");
+      }).to.throw();
     });
+  });
+
+  context("caracteristicas", function() {
+    it("should return empty array", function() {
+      const sut = new grua("miGrua");
+      expect(sut.caracteristicas)
+        .to.be.an("array")
+        .lengthOf(0);
+    });
+  });
 });
